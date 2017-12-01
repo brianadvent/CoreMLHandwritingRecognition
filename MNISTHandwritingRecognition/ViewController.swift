9643247067373484742c8bbd08667c7a3112255d
Brian Advent <youtube@brianadvent.com>
2017-12-01 17:52:34 +0100

    Initial Commit

---
+++ MNISTHandwritingRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MNISTHandwritingRecognition
+//
+//  Created by Brian Advent on 01.12.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
