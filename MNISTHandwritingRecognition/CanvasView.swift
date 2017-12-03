257fef55e86136da5962f87f4ddbe29caec5e8aa
Brian Advent <youtube@brianadvent.com>
2017-12-03 15:38:39 +0100

    Add missing files

---
+++ MNISTHandwritingRecognition/CanvasView.swift
@@ -0,0 +1,98 @@
+//
+//  CanvasView.swift
+//  Canvas
+//
+//  Created by Brian Advent on 01.12.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    // Properties for line drawing
+    var lineColor:UIColor!
+    var lineWidth:CGFloat!
+    var path:UIBezierPath!
+    var touchPoint:CGPoint!
+    var startingPoint:CGPoint!
+    
+    override func layoutSubviews() {
+        self.clipsToBounds = true // no lines should be visible outside of the view
+        self.isMultipleTouchEnabled = false // we only process one touch at a time
+        
+        // standard settings for our line
+        lineColor = UIColor.white
+        lineWidth = 10
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // get the touch position when user starts drawing
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // get the next touch point as the user draws
+        let touch = touches.first
+        touchPoint = touch?.location(in: self)
+        
+        // create path originating from the starting point to the next point the user reached
+        path = UIBezierPath()
+        path.move(to: startingPoint)
+        path.addLine(to: touchPoint)
+        
+        // setting the startingPoint to the previous touchpoint
+        // this updates while the user draws
+        startingPoint = touchPoint
+        
+        drawShapeLayer() // draws the actual line shapes
+    }
+    
+    func drawShapeLayer() {
+        
+        let shapeLayer = CAShapeLayer()
+        // the shape layer is used to draw along the already created path
+        shapeLayer.path = path.cgPath
+        
+        // adjusting the shape to our wishes
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        
+        // adding the shapelayer to the vies layer and forcing a redraw
+        self.layer.addSublayer(shapeLayer)
+        self.setNeedsDisplay()
+        
+    }
+    
+    func clearCanvas() {
+        path.removeAllPoints()
+        self.layer.sublayers = nil
+        self.setNeedsDisplay()
+    }
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+extension UIImage {
+    // UIImage extension that creates a UIImage from a UIView
+    convenience init (view:UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: image!.cgImage!)
+    }
+
+}
+
--- MNISTHandwritingRecognition/ViewController.swift
+++ MNISTHandwritingRecognition/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    // Outlets for UI Elements
+    @IBOutlet weak var digitLabel: UILabel!
+    @IBOutlet weak var canvasView: CanvasView!
+    
+    
+    var requests = [VNRequest]() // holds Image Classification Request
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupVision()
     }
 
+    func setupVision() {
+        // load MNIST model for the use with the Vision framework
+        guard let visionModel = try? VNCoreMLModel(for: MNIST().model) else {fatalError("can not load Vision ML model")}
+        
+        // create a classification request and tell it to call handleClassification once its done
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: self.handleClassification)
+        
+        self.requests = [classificationRequest] // assigns the classificationRequest to the global requests array
+        
+    }
+    
+    func handleClassification (request:VNRequest, error:Error?) {
+        guard let observations = request.results else {print("no results"); return}
+        
+        // process the ovservations
+        let classifications = observations
+            .flatMap({$0 as? VNClassificationObservation}) // cast all elements to VNClassificationObservation objects
+            .filter({$0.confidence > 0.8}) // only choose observations with a confidence of more than 80%
+            .map({$0.identifier}) // only choose the identifier string to be placed into the classifications array
+        
+        DispatchQueue.main.async {
+            self.digitLabel.text = classifications.first // update the UI with the classification
+        }
+        
+    }
+    
+    @IBAction func clearCanvas(_ sender: Any) {
+        canvasView.clearCanvas()
+    }
+    
+    @IBAction func recognizeDigit(_ sender: Any) {
+        let image = UIImage(view: canvasView) // get UIImage from CanvasView
+        let scaledImage = scaleImage(image: image, toSize: CGSize(width: 28, height: 28)) // scale the image to the required size of 28x28 for better recognition results
+        
+        let imageRequestHandler = VNImageRequestHandler(cgImage: scaledImage.cgImage!, options: [:]) // create a handler that should perform the vision request
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+        }catch{
+            print(error)
+        }
+        
+    }
+    
+    // scales any UIImage to a desired target size
+    func scaleImage (image:UIImage, toSize size:CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
